67edb12f97c731822ac48844f8405b9ceb1c6651
Miguel Lucas <miguelluc@ymail.com>
2017-08-23 05:41:00 +0000

    added the user profile link to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   resources :places do
     resources :comments, only: :create
+    resources :photos, only: :create
   end
+
+  resources :users, only: :show
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
